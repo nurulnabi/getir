b6bcb500d6d0855b92f21ffff3636a4409529333
unknown <noor.csarchitect@gmail.com>
2021-10-01 20:49:41 +0530

    created serer file

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const middlewares = require('./src/middlewares');
+const routes = require('./src/routes');
+const mongoConnector = require('./common/mongoConnector');
+const app = express();
+
+async function bootstrap() {
+	app.use(bodyParser.json());
+	app.use(middlewares);
+	app.use(routes);
+
+	await mongoConnector.init();
+	let port = process.env.PORT || 5000;
+	app.listen(port, function() {
+		console.log('server started on port:', port);
+	})
+
+	return app;
+}
+
+
+module.exports = bootstrap();
